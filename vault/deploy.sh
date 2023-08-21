b37cb87f2161098dd4575f7e8d1601fbc7d4aeb6
EPE <epe@das.be>
2023-08-21 17:40:13 +0200

    test create deploy.sh

--- vault/deploy.sh
+++ vault/deploy.sh
@@ -14,7 +14,7 @@ echo "Attente de démarrage de Vault..."
 sleep 10
 
 # Initialisez Vault et stockez les clés de déchiffrement dans un fichier temporaire
-docker exec vault vault operator init -key-shares=3 -key-threshold=2 > keys.txt
+docker exec vault vault operator init -dev -key-shares=3 -key-threshold=2 > keys.txt
 
 # Déverrouillez Vault avec la clé de déchiffrement principale
 cat keys.txt | grep "Unseal Key 1" | awk '{print $4}' | docker exec -i vault vault operator unseal -
@@ -24,10 +24,10 @@ cat keys.txt | grep "Unseal Key 2" | awk '{print $4}' | docker exec -i vault vau
 cat keys.txt | grep "Initial Root Token" | awk '{print $4}' | docker exec -i vault vault login -
 
 # Activez le moteur de stockage de secrets KV
-docker exec vault vault secrets enable -path=mysecrets kv
+docker exec vault vault -dev secrets enable -path=mysecrets kv
 
 # Créez un secret
-docker exec vault vault kv put mysecrets/mysecretkey myvalue=mysecretvalue
+docker exec vault vault -dev kv put mysecrets/mysecretkey myvalue=mysecretvalue
 
 # Nettoyez le fichier temporaire contenant les clés
 rm keys.txt
