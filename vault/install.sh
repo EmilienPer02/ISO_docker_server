24cb816b5eba8315d7b69be3e92b44fc50e47177
EPE <epe@das.be>
2023-08-22 15:53:40 +0200

    retry after rebase

--- vault/install.sh
+++ vault/install.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
-openssl req -x509 -newkey rsa:4096 -keyout ./vault_data/key.pem -out ./vault_data/certificate.pem -sha256 -days 3650 -nodes -subj "/ST=ISO_Server/L=ISO_Server/O=ISO_Server/OU=ISO_Server"
+openssl req -x509 -newkey rsa:4096 -keyout ./vault_data/key.pem -out ./vault_data/certificate.pem -sha256 -days 3650 -nodes -addext 'subjectAltName = IP:127.0.0.1' -subj "/ST=ISO_Server/L=ISO_Server/O=ISO_Server/OU=ISO_Server/IP=127.0.0.1"
+docker-compose up -d
