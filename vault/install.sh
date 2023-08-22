3908c0bdd12854357ad93fa2ca0950b39f30e0d1
EPE <epe@das.be>
2023-08-22 09:54:09 +0200

    test deploy vault in prod

--- vault/install.sh
+++ vault/install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 # docker volume create vault-volume
-openssl req -x509 -newkey rsa:4096 -keyout ./vault-data/key.pem -out ./vault-data/certificate.pem -sha256 -days 3650 -nodes -subj "/C=ISO_DAS_Server/ST=ISO_DAS_Server/L=ISO_DAS_Server/O=ISO_DAS_Server/OU=ISO_DAS_Server/CN=10.1.20.100"
+openssl req -x509 -newkey rsa:4096 -keyout ./vault-data/key.pem -out ./vault-data/certificate.pem -sha256 -days 3650 -nodes -subj "/C=ISO/ST=ISO_Server/L=ISO_Server/O=ISO_Server/OU=ISO_Server/CN=10.1.20.100"
