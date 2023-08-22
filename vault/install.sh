2f16eda96ad38c428d81d5290f6f4b2dfd85c12d
EPE <epe@das.be>
2023-08-22 11:37:01 +0200

    test deploy vault in prod

--- vault/install.sh
+++ vault/install.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 docker volume create vault-data
 openssl req -x509 -newkey rsa:4096 -keyout ./vault-data/key.pem -out ./vault-data/certificate.pem -sha256 -days 3650 -nodes -subj "/ST=ISO_Server/L=ISO_Server/O=ISO_Server/OU=ISO_Server"
+docker-compose up -d
+docker exec -it vault vault operator init > generated_keys.txt
