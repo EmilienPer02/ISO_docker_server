6b1bfab37f1f0c6227a8535a8f0438082e334043
EPE <epe@das.be>
2023-08-23 16:37:10 +0200

    try to unseal in command line .

--- vault/install.sh
+++ vault/install.sh
@@ -6,7 +6,12 @@ sleep 5
 sudo docker exec -it vault vault operator init -n 6 -t 2 --format=json --tls-skip-verify > key.json
 seal_key_0=$(cat key.json |  jq ".unseal_keys_b64[0]" | sed 's/\"//g' )
 seal_key_1=$(cat key.json | jq ".unseal_keys_b64[1]"| sed 's/\"//g' )
-echo $seal_key_0
-echo $seal_key_1
-docker exec -it vault vault operator unseal  $seal_key_0 --tls-skip-verify
-docker exec -it vault vault operator unseal $seal_key_1 --tls-skip-verify
+root_token=$(cat key.json | jq ".root_token"| sed 's/\"//g' )
+docker exec -it vault vault operator unseal --tls-skip-verify $seal_key_0
+docker exec -it vault vault operator unseal  --tls-skip-verify $seal_key_1
+docker exec -it prodvault vault login --tls-skip-verify $root_token
+docker exec -it vault vault secrets enable --tls-skip-verify -version=2 kv
+echo "----------------------------------------------------------------------------------"
+cat keys.json
+rm key.json
+echo "----------------------------------------------------------------------------------"
