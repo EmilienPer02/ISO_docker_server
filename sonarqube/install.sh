b8e1d9a662568c84a40b833054ebcd49a0059fcb
EPE <epe@das.be>
2023-08-24 17:08:42 +0200

    add network host

--- sonarqube/install.sh
+++ sonarqube/install.sh
@@ -9,7 +9,7 @@ VAULT_ADDR="https://localhost:8200"  # Adresse de votre serveur Vault
 MYSQL_RANDOM_PASSWORD=$(echo $RANDOM | md5sum | head -c 20; echo;)
 
 #Start mysql
-MYSQL_ROOT_PASSWORD=$MYSQL_RANDOM_PASSWORD docker-compose up -d
+ docker-compose up -d -e MYSQL_ROOT_PASSWORD=$MYSQL_RANDOM_PASSWORD
 
 # Activer l'engine "databases" dans Vault
 curl -X POST -H "X-Vault-Token: $VAULT_TOKEN" -d '{"type": "database"}' $VAULT_ADDR/v1/sys/mounts/database -k
