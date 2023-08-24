20de6dab7b6ab7c866d7d2d4b023e915ebc7b1c4
EPE <epe@das.be>
2023-08-24 16:41:36 +0200

    add network host

--- sonarqube/install.sh
+++ sonarqube/install.sh
@@ -5,7 +5,7 @@ VAULT_TOKEN=$1
 #!/bin/bash
 
 # Variables
-VAULT_ADDR="http://localhost:8200"  # Adresse de votre serveur Vault
+VAULT_ADDR="https://localhost:8200"  # Adresse de votre serveur Vault
 VAULT_TOKEN="YOUR_VAULT_TOKEN"      # Votre token d'authentification Vault
 MYSQL_RANDOM_PASSWORD=$(echo $RANDOM | md5sum | head -c 20; echo;)
 
