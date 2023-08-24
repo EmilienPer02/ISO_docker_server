f457081cda8d3331cf576893ac2eea5c98caabd2
EPE <epe@das.be>
2023-08-24 11:24:38 +0200

    clean vault install script and add verbose option

--- vault/install.sh
+++ vault/install.sh
@@ -2,7 +2,7 @@
 mkdir -p logs
 mkdir -p ./vault_data
 now=$(date +"%m_%d_%Y")
-log="/logs/$now.txt"
+log="logs/$now.txt"
 #Create a SSL Certificate for HTTPS
 openssl req -x509 -newkey rsa:4096 -keyout ./vault_data/key.pem -out ./vault_data/certificate.pem -sha256 -days 3650 -nodes -addext 'subjectAltName = IP:127.0.0.1' -subj "/ST=ISO_Server/L=ISO_Server/O=ISO_Server/OU=ISO_Server/IP=127.0.0.1" >$log
 
