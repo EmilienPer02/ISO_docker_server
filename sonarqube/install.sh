dad005c4be05335ed15bebe5e9142bd2b59a146c
EPE <epe@das.be>
2023-08-24 16:35:01 +0200

    add network host

--- sonarqube/install.sh
+++ sonarqube/install.sh
@@ -7,28 +7,15 @@ VAULT_TOKEN=$1
 # Variables
 VAULT_ADDR="http://localhost:8200"  # Adresse de votre serveur Vault
 VAULT_TOKEN="YOUR_VAULT_TOKEN"      # Votre token d'authentification Vault
+MYSQL_RANDOM_PASSWORD=$(echo $RANDOM | md5sum | head -c 20; echo;)
+
+#Start mysql
+MYSQL_ROOT_PASSWORD=$MYSQL_RANDOM_PASSWORD docker-compose
 
 # Activer l'engine "databases" dans Vault
 curl -X POST -H "X-Vault-Token: $VAULT_TOKEN" -d '{"type": "database"}' $VAULT_ADDR/v1/sys/mounts/database -k
 
-# Créer une configuration pour la base de données PostgreSQL
-curl -X POST -H "X-Vault-Token: $VAULT_TOKEN" -d '{
-  "plugin_name": "postgresql-database-plugin",
-  "allowed_roles": "myapp-role",
-  "connection_url": "postgresql://{{username}}:{{password}}@localhost:5432/sonarqube?sslmode=disable",
-  "username": "myappadmin",
-  "password": "password123"
-}' $VAULT_ADDR/v1/database/config/sonarqubedb -k
-
-# Créer un rôle pour la base de données
-curl -X POST -H "X-Vault-Token: $VAULT_TOKEN" -d '{
-  "db_name": "myappdb",
-  "creation_statements": "CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";",
-  "default_ttl": "1h",
-  "max_ttl": "24h"
-}' $VAULT_ADDR/v1/database/roles/myapp-role
-
-# Activer la rotation automatique du mot de passe pour le rôle
-curl -X POST -H "X-Vault-Token: $VAULT_TOKEN" -d '{
-  "rotation_period": "1h"
-}' $VAULT_ADDR/v1/database/config/myappdb/rotate-root/myapp-role
+curl 'https://10.1.20.100:8200/v1/database/config/mysql' \
+  -H "X-Vault-Token: $VAULT_TOKEN" \
+  -d '{"backend":"database","name":"mysql","plugin_name":"mysql-legacy-database-plugin","verify_connection":true,"connection_url":"{{username}}:{{password}}@tcp(host:3306)/sonar","username":"root","password":"your_root_password","max_open_connections":4,"max_idle_connections":0,"max_connection_lifetime":"0s"}' \
+  -k
