bea2be34ea8243a875d4cc14da469760811387ff
EPE <epe@das.be>
2023-08-24 12:21:47 +0200

    create main script

--- install_all.sh
+++ install_all.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
-#Install vault and get root token
+# Install treafic
+docker-compose -f ./treafic/docker-compose.yml up -d
+
+# Install vault and get root token
 root_token= $(./vault/install.sh | jq ".root_token"| sed 's/\"//g' )
 echo $root_token
