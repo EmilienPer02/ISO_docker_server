73115ee71c051ef2efac741605682926a611c47d
EPE <epe@das.be>
2023-08-24 11:46:57 +0200

    test &>$log

--- vault/install.sh
+++ vault/install.sh
@@ -7,8 +7,8 @@ log="logs/$now.txt"
 openssl req -x509 -newkey rsa:4096 -keyout ./vault_data/key.pem -out ./vault_data/certificate.pem -sha256 -days 3650 -nodes -addext 'subjectAltName = IP:127.0.0.1' -subj "/ST=ISO_Server/L=ISO_Server/O=ISO_Server/OU=ISO_Server/IP=127.0.0.1" 2>$log
 
 #Create and deploy the vault
-docker-compose build >$log 2>$log
-docker-compose up -d >$log 2>$log
+docker-compose build >>$log 2>>$log
+docker-compose up -d >>$log 2>>$log
 
 # Wait to be sure the vault is ready
 sleep 5
@@ -20,12 +20,12 @@ sudo docker exec -it vault vault operator init -n 6 -t 2 --format=json --tls-ski
 seal_key_0=$(cat key.json |  jq ".unseal_keys_b64[0]" | sed 's/\"//g' )
 seal_key_1=$(cat key.json | jq ".unseal_keys_b64[1]"| sed 's/\"//g' )
 root_token=$(cat key.json | jq ".root_token"| sed 's/\"//g' )
-docker exec -it vault vault operator unseal --tls-skip-verify $seal_key_0 >$log 2>$log
-docker exec -it vault vault operator unseal  --tls-skip-verify $seal_key_1 >$log 2>$log
+docker exec -it vault vault operator unseal --tls-skip-verify $seal_key_0 >>$log 2>>$log
+docker exec -it vault vault operator unseal  --tls-skip-verify $seal_key_1 >>$log 2>>$log
 
 # Enabling KV engine
-docker exec -it vault vault login --tls-skip-verify $root_token >$log 2>$log
-docker exec -it vault vault secrets enable --tls-skip-verify -version=2 kv >$log 2>$log
+docker exec -it vault vault login --tls-skip-verify $root_token >>$log 2>>$log
+docker exec -it vault vault secrets enable --tls-skip-verify -version=2 kv >>$log 2>>$log
 
 # show and destroy vault secrets
 cat key.json
