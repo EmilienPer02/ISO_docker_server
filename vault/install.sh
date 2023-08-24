67eec306cb4dc28081a3a8510c3fd9cd863b4b7b
EPE <epe@das.be>
2023-08-24 09:59:42 +0200

    clean vault install script and add verbose option

--- vault/install.sh
+++ vault/install.sh
@@ -20,7 +20,7 @@ while getopts ":v" opt; do
     esac
 done
 
-shift $((OPTIND-1))
+#shift $((OPTIND-1))
 
 #Create a SSL Certificate for HTTPS
 openssl req -x509 -newkey rsa:4096 -keyout ./vault_data/key.pem -out ./vault_data/certificate.pem -sha256 -days 3650 -nodes -addext 'subjectAltName = IP:127.0.0.1' -subj "/ST=ISO_Server/L=ISO_Server/O=ISO_Server/OU=ISO_Server/IP=127.0.0.1" | verbose
