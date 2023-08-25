ee23edf0bb257f0c513a550a6b3de59262a9b0c3
EPE <epe@das.be>
2023-08-25 11:49:21 +0200

    try to run mysql with vault

--- sonarqube/install.sh
+++ sonarqube/install.sh
@@ -10,8 +10,8 @@ MYSQL_RANDOM_PASSWORD=$(echo $RANDOM | md5sum | head -c 12; echo;)
 echo $MYSQL_RANDOM_PASSWORD
 #Start mysql
 docker-compose run --rm -d -e MYSQL_ROOT_PASSWORD=$MYSQL_RANDOM_PASSWORD -p '3306:3306' mysql
-
-# Activer l'engine "databases" dans Vault
-curl -X POST -H "X-Vault-Token: $VAULT_TOKEN" -d '{"type": "database"}' $VAULT_ADDR/v1/sys/mounts/database -k
-sleep 2
-curl $VAULT_ADDR/v1/database/config/mysql   -H "x-vault-token:$VAULT_TOKEN"   --data-raw '{"backend":"database","name":"mysql","plugin_name":"mysql-rds-database-plugin","verify_connection":true,"connection_url":"{{username}}:{{password}}@tcp(host:3306)/sonar","username":"root","password":"'$MYSQL_RANDOM_PASSWORD'","max_open_connections":4,"max_idle_connections":0,"max_connection_lifetime":"0s"}'   --insecure
+cmd=curl $VAULT_ADDR/v1/database/config/mysql   -H "x-vault-token:$VAULT_TOKEN"   --data-raw '{"backend":"database","name":"mysql","plugin_name":"mysql-rds-database-plugin","verify_connection":true,"connection_url":"{{username}}:{{password}}@tcp(host:3306)/sonar","username":"root","password":"'$MYSQL_RANDOM_PASSWORD'","max_open_connections":4,"max_idle_connections":0,"max_connection_lifetime":"0s"}'   --insecure
+if $cmd | grep -q 'no handler for route'; then
+  curl -X POST -H "X-Vault-Token: $VAULT_TOKEN" -d '{"type": "database"}' $VAULT_ADDR/v1/sys/mounts/database -k
+  $cmd
+fi
