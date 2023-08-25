4cc064c64c4a90ecb1bf74b665e3ee84a48b574c
EPE <epe@das.be>
2023-08-25 12:01:34 +0200

    try to run mysql with vault

--- sonarqube/install.sh
+++ sonarqube/install.sh
@@ -13,6 +13,7 @@ docker-compose run --rm -d -e MYSQL_ROOT_PASSWORD=$MYSQL_RANDOM_PASSWORD -p '330
 cmd_enable_database_engine= curl -X POST -H "X-Vault-Token: $VAULT_TOKEN" -d '{"type": "database"}' $VAULT_ADDR/v1/sys/mounts/database -k
 cmd_create_connection=curl $VAULT_ADDR/v1/database/config/mysql   -H "x-vault-token:$VAULT_TOKEN"   --data-raw '{"backend":"database","name":"mysql","plugin_name":"mysql-rds-database-plugin","verify_connection":true,"connection_url":"{{username}}:{{password}}@tcp(host:3306)/sonar","username":"root","password":"'$MYSQL_RANDOM_PASSWORD'","max_open_connections":4,"max_idle_connections":0,"max_connection_lifetime":"0s"}'   --insecure
 cmd_create_connection_output=$(eval $cmd_create_connection)
+echo "------------------------>"$cmd_create_connection_output
 
 if [ $? -ne 0 ] || $( $cmd_create_connection_output | grep -q "no handler for route"); then
   echo "La commande cmd_create_connection a échoué ou contient 'no handler for route'. Exécution de cmd_enable_database_engine."
